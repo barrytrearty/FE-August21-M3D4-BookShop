b4cbef48265c4e94e54abfe9d37135a2688d979e
barrytrearty <btrearty@gmail.com>
2021-08-12 18:54:00 +0100

    Changed to using event.target and closest

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 "strict mode";
 
+let booksArray;
 const mainBookSection = document.getElementById("main-book-section");
 
-//Code for cart
-let cart = [];
-const cartNumber = document.getElementById("cart-number");
-cartNumber.innerHTML = `${cart.length}`;
+fetch("https://striveschool-api.herokuapp.com/books")
+  .then((response) => response.json())
+  .then((books) => {
+    console.log(books);
+    booksArray = books;
+    booksArray.forEach((book) => displayBooks(book));
+    //   const cartButtons = document.getElementsByClassName("cart-button");
+    //   cartButtons.map((button) =>
+    //     button.addEventListener("click", addBookToCart)
+    //   );
+  })
+  .catch((err) => console.log(err));
+
+const filterBooks = function (query) {
+  displayBooks(booksArray.filter((book) => book.title.includes(query)));
+};
 
 const displayBooks = function (bookObject) {
   let newBookCard = document.createElement("div");
   newBookCard.classList.add("col");
-  newBookCard.id = bookObject.asin;
+  newBookCard.id = Number(bookObject.asin);
   newBookCard.innerHTML = `<div class="card mt-3">
   <img src="${bookObject.img}" class="card-img-top" alt="...">
   <div class="card-body">
@@ -19,16 +32,23 @@ const displayBooks = function (bookObject) {
         <h4>${bookObject.price}</h4>
         <div class="buttons-sect"> 
             <button id="${bookObject.asin}button" class="btn btn-success cart-button" onclick="addItemToCart(${bookObject.asin})" ><i class="bi bi-cart-fill"></i> </button>
-            <button  class="btn btn-danger" onclick="skipItem(${bookObject.asin})"><i class="bi bi-trash"></i> </button>
+            <button  class="btn btn-danger" onclick="skipItem(event)"><i class="bi bi-trash"></i> </button>
         </div>
     </div>
   </div></div>`;
   mainBookSection.appendChild(newBookCard);
 };
 
-const skipItem = function (bookId) {
-  console.log(bookId);
-  document.getElementById(bookId).remove();
+//Code for cart
+let cart = [];
+const cartNumber = document.getElementById("cart-number");
+cartNumber.innerHTML = `${cart.length}`;
+
+const skipItem = function (event) {
+  //   console.log(bookId);
+  //   document.getElementById(bookId).remove();
+  console.log(event.target.closest(".col"));
+  event.target.closest(".col").remove();
 };
 
 const addItemToCart = function (bookId) {
@@ -45,26 +65,20 @@ const addItemToCart = function (bookId) {
   console.log(cart);
 };
 
+//PROBLEM WITH THIS - map wont work, for/of wont work, try regular for?
 const emptyCart = function () {
-  //   let selectedItems = document.getElementsByClassName("selected");
-  //   selectedItems.map((item) => item.classList.remove("selected"));
+  let selectedItems = document.getElementsByClassName("selected");
+  console.log(selectedItems);
+  for (item of selectedItems) {
+    item.classList.remove("selected");
+  }
+  console.log(selectedItems);
+  //   selectedItemsInCart.map((item) => item.classList.remove("selected"));
   cart = [];
   cartNumber.innerHTML = `${cart.length}`;
   console.log(cart);
 };
 
-const getBooks = function () {
-  fetch("https://striveschool-api.herokuapp.com/books")
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data);
-      data.forEach((book) => displayBooks(book));
-      //   const cartButtons = document.getElementsByClassName("cart-button");
-      //   cartButtons.map((button) =>
-      //     button.addEventListener("click", addBookToCart)
-      //   );
-    })
-    .catch((err) => console.log(err));
-};
+// const getBooks = function () {};
 
-getBooks();
+// getBooks();
